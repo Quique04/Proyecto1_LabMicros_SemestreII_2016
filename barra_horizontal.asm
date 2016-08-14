f9e2bbc43cc34e3b38e5b8c5d0d3d5683a84429e
gjmabo <gjmabo@gmail.com>
2016-08-14 10:17:16 -0600

    Update barra_horizontal.asm

--- barra_horizontal.asm
+++ barra_horizontal.asm
@@ -3,7 +3,6 @@
 ;El-5413 Lab. de Estructuras de Microprocesadores
 ;Proyecto 1 - Arkanoid - Barra horizontal
 
-
 ;-------------------------------Descripcion General-----------------------------
 ;El siguiente codigo imprime en pantalla una barra horizontal como fue
 ;especificada en el instructivo del primer proyecto. Haciendo uso de una
@@ -11,6 +10,69 @@
 
 ;###############################################################################
 
+
+;-------------------------------------Macros-----------------------------------
+
+%macro Escribir 2
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, %1
+        mov rdx, %2
+        syscall
+%endmacro
+
+
+%macro Sumar_Barra 2
+        cmp %2, 56      ;54 => ascii decimal para '6'; compara unidades(posicion vs limite);%2 = pos_barra_uni
+        jz comp_dec_lim
+        jnz comp_uni_tope       ;tope => 9
+
+comp_dec_lim:
+        cmp %1, 50              ;50 => ascii decimal para '2'; compara decenas(posicion vs limite);%1 = pos_barra_dec
+        jz final_macro_suma	;llego al tope, no hace nada
+        jnz suma_1_uni
+
+comp_uni_tope:
+        cmp %2, 57              ;57 =>ascii decimal para '9'; compara unidades(posiciones vs tope)
+        jz suma_1_dec           ;suma 1 en las decenas(%1) y mueve un '0' (48) a las unidades (%2)
+        jnz suma_1_uni
+
+suma_1_uni:
+        inc %2
+        jmp final_macro_suma
+
+suma_1_dec:
+        mov %2, 48
+        inc %1
+        jmp final_macro_suma
+
+final_macro_suma:
+%endmacro
+
+
+%macro Restar_Barra 2
+        cmp %1, 48      	;48 => ascii decimal para '0'; compara decenas(posicion vs tope);%1 = pos_barra_dec
+        jz comp_uni_tope_resta       ;pregunta si %1 es igual a '0'
+        jnz resta_1_uni         ;resta 1 a %2;%2 = pos_barra_dec
+
+comp_uni_tope_resta:
+        cmp %2, 52      ;52 => ascii decimal para '4'; compara decenas(posicion vs tope);%2 = pos_barra_uni
+        jz final_macro_resta
+        jnz resta_1_uni
+
+resta_1_uni:
+        cmp %2, 48
+	jz resta_1_dec
+	dec %2
+        jmp final_macro_resta
+
+resta_1_dec:
+        mov %2, 57
+        dec %1
+        jmp final_macro_resta
+
+final_macro_resta:
+%endmacro
 
 ;--------------------------------Segmento de Datos-----------------------------
 ;Aqui se declaran las variables utilizadas en el programa
@@ -22,19 +84,19 @@
 
 section .data
 ;------------------------------------Variables---------------------------------
-	pos_barra_uni: dw 50			;posicion de la barra en la pantalla, valor inicial 12
-	pos_barra_dec: dw 49			;se deben definir como caracteres de decenas y unidades
+	;pos_barra_uni: db 50			;posicion de la barra en la pantalla, valor inicial 12
+	;pos_barra_dec: db 49			;se deben definir como caracteres de decenas y unidades (no se usan)
 	;especificamente 'pos_barra' es la posicion del extremo izquierdo de la barra
 	;Codigos ascii para numeros de 0 a 9:
 	;0 => 48 => 0x30
 	;9 => 57 => 0x39
 
-;segment .bss					;Aun no es necesario
-
-
 ;-----------------------------------Constantes---------------------------------
 	borrar: db 27,"[2J"
 	borrar_tam: equ $-borrar
+
+        cons_banner: db "Presione una tecla, seguida de Enter: "                ;Primer mensaje para el usuario
+        cons_tam_banner: equ $-cons_banner
 
 	lineahoriz_sup: db 27,"[1;1f ","*"
 	lineahoriz_sup_tam: equ $-lineahoriz_sup
@@ -57,17 +119,30 @@ section .data
 	finalhor_sup: db 27,"[1;32f ","*"
 	finalhor_sup_tam: equ $-finalhor_sup
 
-	finalhor_inf: db 27,"[34;32f ","*"
+	finalhor_inf: db 27,"[34;32f ","* "
 	finalhor_inf_tam: equ $-finalhor_inf
 
-	final_marco: db 27,"[34;33f "
+	final_marco: db 27,"[35;1f "
 	final_marco_tam: equ $-final_marco
 
-	barra_horiz_pos: db 27,"[28;12f "
+	barra_horiz_pos: db 27,"[32;10f ","------"			;se debe dejar el '-----'
 	barra_horiz_pos_tam: equ $-barra_horiz_pos
 
 	barra_horiz: db "o===o"
 	barra_horiz_tam: equ $-barra_horiz
+
+;	dato_ent: db ""
+;	dato_ent_tam: equ $-dato_ent
+
+;----------------------------Variables para apagar y encender el
+;----------------------------modo canonico y el echo
+	termios:        times 36 db 0
+	stdin:          equ 0
+	ICANON:         equ 1<<1
+	ECHO:           equ 1<<3
+
+segment .bss                                    ;Aun no es necesario
+        dato_ent: resb 1
 
 ;----------------------------------Segmento de Codigo---------------------------
 section .text
@@ -76,75 +151,7 @@ section .text
 	global _start
 	global _ciclo_juego
 	global _salir
-
-;-----------Macros--------------
-%macro Escribir 2
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, %1
-	mov rdx, %2
-	syscall
-%endmacro
-
-%macro Sumar_Barra 2
-	cmp dword [%2], 54	;54 => ascii decimal para '6'; compara unidades(posicion vs limite);%2 = pos_barra_uni
-	je comp_dec_lim
-	jne comp_uni_tope	;tope => 9
-
-comp_dec_lim:
-	cmp dword [%1], 50		;50 => ascii decimal para '2'; compara decenas(posicion vs limite);%1 = pos_barra_dec
-	je set_zero_total
-	jne suma_1_uni
-
-comp_uni_tope:
-	cmp dword [%2], 57		;57 =>ascii decimal para '9'; compara unidades(posiciones vs tope)
-	je suma_1_dec		;suma 1 en las decenas(%1) y mueve un '0' (48) a las unidades (%2)
-	jne suma_1_uni
-
-suma_1_uni:
-	inc dword [%2]
-	jmp final_macro_suma
-
-suma_1_dec:
-	mov dword [%2], 48
-	inc dword [%1]
-	jmp final_macro_suma
-
-set_zero_total:
-	mov dword [%2], 48
-	mov dword [%1], 48
-	jmp final_macro_suma
-
-final_macro_suma:
-%endmacro
-
-%macro Restar_Barra 2
-	cmp dword [%2], 48	;48 => ascii decimal para '0'; compara unidades(posicion vs tope);%2 = pos_barra_uni
-        je comp_dece_tope	;pregunta si %1 es igual a '0'
-        jne resta_1_uni		;resta 1 a %2
-
-comp_dece_tope:
-        cmp dword [%1], 48	;48 => ascii decimal para '0'; compara decenas(posicion vs tope);%1 = pos_barra_dec
-        je final_macro_resta
-        jne resta_1_dec
-
-resta_1_uni:
-        dec dword [%2]
-        jmp final_macro_resta
-
-resta_1_dec:
-        mov dword [%2], 57
-        dec dword [%1]
-        jmp final_macro_resta
-
-final_macro_resta:
-%endmacro
-
-%macro Salida 0
-	mov rax, 60
-	mov rdi, 0
-	syscall
-%endmacro
+	global _parada
 
 ;--------Procedimientos---------
 Proc_Marco:				;Subrutina para pintar el marco del juego
@@ -204,42 +211,34 @@ ret					;Final del procedimiento 'Marco'
 
 Proc_GetKey:				;Subrutina para obtener el codigo de la
 					;tecla presionada (si se presiono alguna)
-	jmp CheckForKey
-CheckForKey:
-	mov ah, 0x01			;pregunta el estado del buffer del teclado, si hay o no una tecla lista
-	int 0x16			;interrupciones de uso de teclado
-	jnz KeyReady			;si la hay, pasa a leer cual tecla es
-	jz ContinueMain			;si no la hay, sigue con el programa principal
-
-KeyReady:
-	mov ah, 0x00			;lee el buffer del teclado
-	int 0x16			;interrupciones de uso del teclado
-;	mov r10, rax			;mueve al registro r10 el resultado de la interrupcion (puede no ser
-					;necesario, por eso se deja como comentario)
-	;En este punto el registro 'al' ya contiene el valor de la tecla
-	jmp ContinueMain		;salta al programa principal
-
-ContinueMain:
+	Escribir cons_banner, cons_tam_banner
+	mov rax, 0				;rax = "sys_read"
+	mov rdi, 0				;rdi = 0 (standard input = teclado)
+	mov rsi, dato_ent			;rsi = dir de memoria donde se guarda el dato de entrada
+	mov rdx, 1				;rdx = 1 =>  cuantos eventos o teclazos capturar
+	syscall
 ret					;Final del procedimiento 'Proc_GetKey'
 ;-----------------------------------------------------
 
 Proc_DecoKey:				;Procedimiento para determinar que tecla se obtuvo
 					;se debe ejecutar inmediatamente despues de Proc_GetKey si se obtuvo el valor de una tecla
-	cmp al, 99
-	je tecla_c			;se mueve hacia la izquierda
+	cmp dword [dato_ent], 99
+	jz tecla_c			;se mueve hacia la izquierda
 
-	cmp al, 122
-	je tecla_z			;se mueve hacia la derecha
+	cmp dword [dato_ent], 122
+	jz tecla_z			;se mueve hacia la derecha
 
-	cmp al, 32			;sale del ciclo '_ciclo_juego';32 => space
-	je tecla_space
+	cmp dword [dato_ent], 32			;sale del ciclo '_ciclo_juego';32 => space
+	jz tecla_space
+
+	jnz Continue
 
 tecla_c:
-	Sumar_Barra pos_barra_dec, pos_barra_uni
+	Sumar_Barra r13, r14
 	jmp Continue
 
 tecla_z:
-	Restar_Barra pos_barra_dec, pos_barra_uni
+	Restar_Barra r13, r14
 	jmp Continue
 
 tecla_space:
@@ -252,45 +251,154 @@ ret
 
 Proc_ImprimeBarra:			;Procedimiento para imprimir la barra movil
 					;llama al macro Escribir luego de modificar los valores del string 'barra_horiz_pos'
-	mov dword [barra_horiz_pos + 0x5], pos_barra_dec
-	mov dword [barra_horiz_pos + 0x6], pos_barra_uni
+	mov [barra_horiz_pos + 0x5], r13
+	mov [barra_horiz_pos + 0x6], r14
+	mov [barra_horiz_pos + 0x7], r15
 	Escribir barra_horiz_pos, barra_horiz_pos_tam
 	Escribir barra_horiz, barra_horiz_tam
 	Escribir final_marco, final_marco_tam
 ret
 ;-------------------------------------------------------
 
+canonical_off:
+        call read_stdin_termios
+
+        ; clear canonical bit in local mode flags
+        push rax
+        mov eax, ICANON
+        not eax
+        and [termios+12], eax
+        pop rax
+
+        call write_stdin_termios
+        ret
+
+;-------------------------------------------------------
+
+echo_off:
+        call read_stdin_termios
+
+        ; clear echo bit in local mode flags
+        push rax
+        mov eax, ECHO
+        not eax
+        and [termios+12], eax
+        pop rax
+
+        call write_stdin_termios
+        ret
+
+;-------------------------------------------------------
+
+canonical_on:
+        call read_stdin_termios
+
+        ; set canonical bit in local mode flags
+        or dword [termios+12], ICANON
+
+        call write_stdin_termios
+        ret
+
+;-------------------------------------------------------
+
+echo_on:
+        call read_stdin_termios
+
+        ; set echo bit in local mode flags
+        or dword [termios+12], ECHO
+
+        call write_stdin_termios
+        ret
+
+;-------------------------------------------------------
+
+read_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov eax, 36h
+        mov ebx, stdin
+        mov ecx, 5401h
+        mov edx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+
+;-------------------------------------------------------
+
+write_stdin_termios:
+        push rax
+        push rbx
+        push rcx
+        push rdx
+
+        mov eax, 36h
+        mov ebx, stdin
+        mov ecx, 5402h
+        mov edx, termios
+        int 80h
+
+        pop rdx
+        pop rcx
+        pop rbx
+        pop rax
+        ret
+
+;-------------------------------------------------------
+
 ;---------Rutina Principal------
 _start:
 	mov r10, 5
+	mov r13, 49
+	mov r14, 49
+	mov r15, 102
+
+	call canonical_off
+	call echo_off
+
 	jmp _ciclo_juego
 
 _ciclo_juego:
-	Escribir borrar, borrar_tam	;Limpia la consola
 
-	;Seccion para pintar la barra horizontal
-	call Proc_ImprimeBarra
+	Escribir borrar, borrar_tam	;Limpia la consola
 
 	;Seccion para pintar el marco del juego
 	;Se utiliza un procedimiento denominado 'Proc_Marco'
 	call Proc_Marco
 
+	call Proc_ImprimeBarra
+
 	;Seccion para tomar la tecla presionada en el teclado
 	call Proc_GetKey
+	mov r12, [dato_ent]
 
+_parada:
 	;Comparar para saber si se debe hacer una decodificacion de la tecla
 	;o si se debe seguir con el programa
 	call Proc_DecoKey
 
 	;Escribir la barra horizontal con los parametros modificados
-	call Proc_ImprimeBarra
+        call Proc_ImprimeBarra
 
 	;Determinar si se sigue o no en el juego, se sale si r10 != 25
 	cmp r10, 25
-	je _salir
-	jne _ciclo_juego
+	jz _salir
+	jnz _ciclo_juego
 
+;----------Salida-----------
 _salir:
-	Salida
+	call canonical_on
+	call echo_on
+
+	mov rax, 60
+        mov rdi, 0
+        syscall
+
 	;Fin de la rutina principal
 ;------------------------------
